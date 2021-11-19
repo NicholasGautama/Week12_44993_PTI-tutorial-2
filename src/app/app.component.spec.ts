6e5a36b3a71bf9fc24aa18ec96827d9883df37e5
NicholasGautama <nicholas.kusumahadi@student.umn.ac.id>
2021-11-19 17:11:59 +0700

    commit_second_tutorial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-second-tutorial'`, () => {
+  it(`should have as title 'ptiweek11-tuto2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-second-tutorial');
+    expect(app.title).toEqual('ptiweek11-tuto2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-second-tutorial app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('ptiweek11-tuto2 app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms'; //import modul reactive form
+import { DataComponent } from './data/data.component';
+import { CommonModule } from '@angular/common'; //taruh juga common module agar angular directive
+											   //bisa dibaca oleh tag normal html seperti div, dll										
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule, //taruh reactive form yg mau diimport
+    CommonModule //taruh commonmodule yang mau diimport
   ],
   providers: [],
   bootstrap: [AppComponent]
